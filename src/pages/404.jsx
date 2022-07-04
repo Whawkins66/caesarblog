783f93018bea4d8e98b6ed45f610022fba67284b
William Hawkins <junk@cloudcaesar.com>
2022-07-04 14:37:19 -0600

    delete 404

--- src/pages/404.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-
-const NotFound = () => (
-    <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
-)
-
-export default NotFound
