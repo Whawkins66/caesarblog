1acf9180f5d34ac6d4528c15a094fd7570e184d8
William Hawkins <junk@cloudcaesar.com>
2022-07-04 18:28:23 -0600

    about

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>I am Caesar!</h1>
+      <p>Veni Vedi Vici.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
